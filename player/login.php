6f89f1d2b3a5f9828515fcc3e8b9464d4678b995
DimonXm <dmensikov108@gmail.com>
2025-03-28 10:59:03 +0400

    First Commit

--- player/login.php
+++ player/login.php
@@ -33,11 +33,15 @@
                     setcookie("user", $this->userMail, time() + 3600, "/");
                     header("Location: $this->url");
                 }else {
-                    $this->userMessage = "Пароль неверный";
+                    $this->userMessage = '  <div class="unknown_password_and_email">
+                                            Unknown password
+                                            </div>';
                     echo $this->userMessage;
                 }
             }if($this->stmt->num_rows == 0){
-                $this->userMessage = "Такой почты нет";
+                $this->userMessage = '  <div class="unknown_password_and_email">
+                                            Unknown email
+                                            </div>';
                 echo $this->userMessage;
             }
         }
@@ -131,6 +135,31 @@
         font-family: "DM Sans";
         font-weight: 400;
         margin-top: -30px;
+    }
+
+    
+    .unknown_password_and_email{
+        position: absolute;
+        font-size: 20px;
+        font-family: "DM Sans";
+        width: 20%;
+        left: 40%;
+        top: 2%;
+        height: 5%;
+        color: white;
+        background-image: none;
+        background-color: rgb(235, 67, 67);
+        border-radius: 20px;
+        animation-name: slidein;
+        animation-duration: 0.5s;
+    }
+    @keyframes slidein {
+        from {
+            top: 0;
+        }
+        to {
+            top: 2%;
+        }
     }
 </style>
 
